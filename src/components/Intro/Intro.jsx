90a07b9007bed89b609e0a045aa7d08e51735ca7
anandu.reghu <anandu.reghu@qburst.com>
2022-11-05 20:58:45 +0530

    implemented intro page

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,8 +1,44 @@
-import React from "react";
+import { init } from "ityped";
+import React, { useEffect, useRef } from "react";
 import "./Intro.scss";
 
 const Intro = () => {
-  return <div className="intro" id="intro">Intro</div>;
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 50,
+      strings: ["Developer", "Designer", "Content Creator"],
+    });
+  }, []);
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imageContainer">
+          <img
+            src="https://raw.githubusercontent.com/safak/youtube/react-portfolio/public/assets/man.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Anandu Reghu</h1>
+          <h3>
+            Freelance <span ref={textRef}>Designer</span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img
+            src="https://github.com/safak/youtube/blob/react-portfolio/public/assets/down.png?raw=true"
+            alt=""
+          />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Intro;
